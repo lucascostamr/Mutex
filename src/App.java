39a38f0ef1c2d6e006369b08200de26079e26edf
DreaMagici4n <lucascostamr812@gmail.com>
2023-11-21 08:39:19 -0300

    chore: adicionando etapas para desenvolver o projeto

--- src/App.java
+++ src/App.java
@@ -1,3 +1,20 @@
+/*
+    [] Criar um limite de tempo para o filosofo/chines comer
+        [] Caso coma menos que o esperado mostrar que estar com fome
+        [] Caso nao como nada morre de fome
+    [] Mostrar quantos filosofos estao comendo/pensando
+        [] printar no console quais hashis estao ocupados
+        [] printar no cosnole quais filosofos estao meditando
+        [] printar no console quais filosofos estao com fome, mais de um dia sem comer
+        [] mostrar filsofos que solicitaram o hashi no momento
+    [] parar quando um dos filosofos morrer de fome
+    [] Caso um filoso passe mais de um dia comendo
+        [] Interromper
+        [] Printar menssagem. "Filosofo x comeu demasiadamente"
+*/
+
+
+
 import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.Semaphore;
